6ef1cb0c08747e5563fb93e492c6f4eafd70c64a
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-10-06 13:15:11 -0500

    Change initialRouteName from 'Map' to 'Home' for better user experience.

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Map">
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Map" component={MapScreen} />
       </Stack.Navigator>

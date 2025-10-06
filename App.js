b75c84a4743db3222dae0ed280391496fc8c40cb
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-10-06 13:13:37 -0500

    feat: initial commit for App.js

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Map" component={MapScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
